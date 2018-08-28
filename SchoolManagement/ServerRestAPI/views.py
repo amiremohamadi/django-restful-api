20a49fa616cb6b39c5a698c20543e332e1138eb4
amir <amiremohamadi@yahoo.com>
2018-08-28 12:51:13 +0430

    better views

--- SchoolManagement/ServerRestAPI/views.py
+++ SchoolManagement/ServerRestAPI/views.py
@@ -27,13 +27,44 @@ def add_grade(request):
 
     if teacher != None:
         # find the student
-        student = get_object_or_404(Student, username=stu_username)
-        lecture = get_object_or_404(Lecture, name=lec_name)
-        teacher_lecture = get_object_or_404(TeacherLecture, TeacherID=teacher, LectureID=lecture)
-        stu_lectute = get_object_or_404(StudentLecture, StudentID=student, TeacherLectureID=teacher_lecture)
+        try:    student = Student.objects.get(username=stu_username)
+        except: return JsonResponse(
+            {'message' : 'student not found!'}, 
+            encoder=JSONEncoder, 
+            status=404)
+        # find the lecture
+        try:    lecture = Lecture.objects.get(name=lec_name)
+        except: return JsonResponse(
+            {'message' : 'lecture not found!'}, 
+            encoder=JSONEncoder, 
+            status=404)
+        # find if teacher has this lecture
+        try:    teacher_lecture = TeacherLecture.objects.get(
+            TeacherID=teacher, 
+            LectureID=lecture)
+        except: return JsonResponse(
+            {'message' : 'teacher has no lecture called %s' %(lec_name)}, 
+            encoder=JSONEncoder, 
+            status=404)
+        # find if student has this lecture        
+        try:    stu_lecture = StudentLecture.objects.get(
+            StudentID=student, 
+            TeacherLectureID=teacher_lecture)
+        except: return JsonResponse(
+            {'message' : 'student has no lecture called %s' %(lec_name)}, 
+            encoder=JSONEncoder)
 
-        stu_lectute.grade = grade
-        stu_lectute.save()
+        # data validation
+        if int(grade) > 20:     grade = '20'
+        elif int(grade) < 0:    grade = '0'
+        # do changes and save
+        stu_lecture.grade = grade
+        stu_lecture.save()
+    
+    else:   return JsonResponse(
+        {'message' : 'can\'t login!'}, 
+        encoder=JSONEncoder, status=401)
 
-    data ={'status': 'ok'}
-    return JsonResponse(data, encoder=JSONEncoder)
+    return JsonResponse(
+        {'message': 'successful'}, 
+        encoder=JSONEncoder, status=200)
