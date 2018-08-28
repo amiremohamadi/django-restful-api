4e5690b3e65dd3a58a8873f0c2c7c0b0d0664d70
amir <amiremohamadi@yahoo.com>
2018-08-28 13:18:33 +0430

    update urls

--- SchoolManagement/ServerRestAPI/urls.py
+++ SchoolManagement/ServerRestAPI/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from .views import index, add_grade
+from .views import index, add_grade, get_teacher_lectures
 
 urlpatterns = [
     re_path(r'^$', index),
-    re_path(r'^teacher/add_grade$', add_grade),
+    re_path(r'^teacher/add-grade$', add_grade),
+    re_path(r'^teacher/get-lectures$', get_teacher_lectures),
 ]
