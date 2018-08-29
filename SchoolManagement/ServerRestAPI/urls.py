9ba9e239414dd5040921f98389897178f3104a12
amir <amiremohamadi@yahoo.com>
2018-08-29 12:49:02 +0430

    student can see lectures

--- SchoolManagement/ServerRestAPI/urls.py
+++ SchoolManagement/ServerRestAPI/urls.py
@@ -1,9 +1,13 @@
 from django.urls import re_path
 
-from .views import index, add_grade, get_teacher_lectures
+from .views import (
+    index, add_grade, get_teacher_lectures, 
+    get_student_lectures
+    )
 
 urlpatterns = [
     re_path(r'^$', index),
     re_path(r'^teacher/add-grade$', add_grade),
     re_path(r'^teacher/get-lectures$', get_teacher_lectures),
+    re_path(r'^student/get-lectures$', get_student_lectures),
 ]
